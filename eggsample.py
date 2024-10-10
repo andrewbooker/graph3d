09619c7ce07e260b388f743c3f8fbebcf158199c
Andrew Booker <andrew.booker@iongroup.com>
2024-10-10 13:20:00 +0100

    fixing sample file generator and checking in output so that graph works on checkp-out

--- eggsample.py
+++ eggsample.py
@@ -8,9 +8,9 @@ for z in range(-r, r):
     for x in range(-r, r):
         d.append({"x": 1.0 * x / r, "y": math.sin(math.pi * x * 2 / r) * math.sin(math.pi * z / r), "z": 1.0 * z / r})
 
-print "function eggsample() { return { data: "
-print d
-print "}; }"
+print("function eggsample() { return { data: ")
+print(d)
+print("}; }")
 
 
 
